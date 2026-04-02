8f10c6006f562357e579a8685fb97010eb884d6a
soulflow08 <gambit.lop4r3v@gmail.com>
2026-04-03 02:44:04 +0300

    Initial Flask social network

--- myproject.py
+++ myproject.py
@@ -1,16 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'social_key_123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
-    return '<h1>🎉 СОЦСЕТЬ soulflow08 РАБОТАЕТ!</h1><p>Регистрация и посты в разработке</p>'
+    return '<h1>Социальная сеть работает! /login</h1>'
 
-@app.route('/register')
-@app.route('/login')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return '<h1>Регистрация работает!</h1>'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
+        if User.query.filter_by(username=username).first():
+            return 'Пользователь существует!'
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return '''
+    <form method="post">
+        Username: <input name="username"><br>
+        Password: <input name="password" type="password"><br>
+        <button>Register</button>
+    </form>
+    '''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('index'))
+        return 'Неверный логин/пароль'
+    return '''
+    <form method="post">
+        Username: <input name="username"><br>
+        Password: <input name="password" type="password"><br>
+        <button>Login</button>
+    </form>
+    <p><a href="/register">Register</a></p>
+    '''
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0')
